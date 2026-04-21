ff1f652fcccd45d241c660f67f1a3e43f96416eb
puspitakaban <puspitaakaban@gmail.com>
2026-04-21 00:28:54 -0400

    commit for final presentation

--- detroit_roads.py
+++
@@ -1,9 +0,0 @@
-import osmnx as ox
-
-G = ox.graph_from_place("Detroit, Michigan, USA", network_type="drive")
-nodes, edges = ox.graph_to_gdfs(G)
-
-edges.to_file("detroit_roads.gpkg", driver="GPKG")
-nodes.to_file("detroit_nodes.gpkg", driver="GPKG")
-
-print(f"Nodes: {len(nodes)}, Edges: {len(edges)}")
